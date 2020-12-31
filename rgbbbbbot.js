fa8828b9db156df35ef8feaefa584f2164701612
Your Name <you@example.com>
2020-12-31 12:25:23 +0100

    Initial commit

---
+++ rgbbbbbot.js
@@ -0,0 +1,70 @@
+var fs = require('fs');
+
+var data = JSON.parse(fs.readFileSync('./data.json'));
+
+var roleName = data.roleName;
+
+const { Client } = require('discord.js');
+const Discord = require('discord.js');
+const client = new Client();
+
+client.on('ready', () => {
+    console.log('Paré à vous souiller !');
+});
+
+client.on('message', message => {
+
+    const args = message.content.split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+    arg = args.toString();
+
+    let userRole = message.guild.roles.cache.find(role => role.name === roleName);
+
+    if(message.content === '!rgb') {
+        const rgbEmbed = new Discord.MessageEmbed()
+            .setTitle("RGB STARTED !!!")
+
+        message.channel.send(rgbEmbed);
+        for(i=0;i<9999;i++) {
+            userRole.setColor('#ff0000');
+            userRole.setColor('#fbff00');
+            userRole.setColor('#32ff00');
+            userRole.setColor('#00fbff');
+            userRole.setColor('#0400ff');
+            userRole.setColor('#ff00fb');
+        }
+    }
+
+    if(message.content.startsWith('!roleName')) {
+        if(arg === '' || arg === ' ') {
+            const emptyRoleEmbed = new Discord.MessageEmbed()
+                .setTitle("Empty role !")
+                .setDescription("To start the animation, you must type the name of your role after *!roleName* ! \n For example : *!roleName RGBMASTER* !")
+            
+            message.channel.send(emptyRoleEmbed);
+        }
+        else {
+            let JSONdata ={
+                "token": data.token,
+                "roleName": arg.replace(',', ' ')
+            }
+            fs.writeFileSync('./data.json', JSON.stringify(JSONdata));
+            roleName = arg.replace(',', ' ');
+            const roleNameEmbed = new Discord.MessageEmbed()
+                .setTitle("Role RGB")
+                .setDescription("Nouveau role RGB : " + roleName)
+            
+            message.channel.send(roleNameEmbed);
+        }
+    }
+
+    if(message.content === '!help') {
+        const helpEmbed = new Discord.MessageEmbed()
+            .setTitle("RGBBBBBOT HELP")
+            .setDescription("The **RGBBBBBOT** role must be higher than the role you want to animate !\n Then, type *!roleName* with the name of the role you want to animate ! \n And, just type *!rgb* !")
+
+        message.channel.send(helpEmbed);
+    }
+});
+
+client.login(data.token);
