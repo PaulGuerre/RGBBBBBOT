91150c1f31c63582647d6570fd379bbb9d49ddf4
Paul Guerre <pguerreftb@gmail.com>
2021-03-03 16:34:17 +0100

    Removed code issues

--- rgbbbbbot.js
+++ rgbbbbbot.js
@@ -1,44 +1,44 @@
 var fs = require('fs');
 
-var data = JSON.parse(fs.readFileSync('./data.json'));
+var data = JSON.parse(fs.readFileSync("./data.json"));
 
 var roleName = data.roleName;
 
-const { Client } = require('discord.js');
-const Discord = require('discord.js');
+const { Client } = require("discord.js");
+const Discord = require("discord.js");
 const client = new Client();
 
-client.on('ready', () => {
-    console.log('Started !');
+client.on("ready", () => {
+    console.log("Started !");
 });
 
-client.on('message', message => {
+client.on("message", (message) => {
     //Séparateur de message
     const args = message.content.split(/ +/g);
     const cmd = args.shift().toLowerCase();
     arg = args.toString();
 
-    let userRole = message.guild.roles.cache.find(role => role.name === roleName);
+    let userRole = message.guild.roles.cache.find((role) => role.name === roleName);
 
     //Mise en place du changement de couleur du grade indiqué
-    if(message.content === '!rgb') {
+    if(message.content === "!rgb") {
         const rgbEmbed = new Discord.MessageEmbed()
             .setTitle("RGB STARTED !!!")
 
         message.channel.send(rgbEmbed);
         for(i=0;i<500;i++) {
-            userRole.setColor('#ff0000');
-            userRole.setColor('#fbff00');
-            userRole.setColor('#32ff00');
-            userRole.setColor('#00fbff');
-            userRole.setColor('#0400ff');
-            userRole.setColor('#ff00fb');
+            userRole.setColor("#ff0000");
+            userRole.setColor("#fbff00");
+            userRole.setColor("#32ff00");
+            userRole.setColor("#00fbff");
+            userRole.setColor("#0400ff");
+            userRole.setColor("#ff00fb");
         }
     }
 
     //Récupération du role sur lequel mettre en place le changement de couleur
-    if(message.content.startsWith('!roleName')) {
-        if(arg === '' || arg === ' ') {
+    if(message.content.startsWith("!roleName")) {
+        if(arg === "" || arg === " ") {
             const emptyRoleEmbed = new Discord.MessageEmbed()
                 .setTitle("Empty role !")
                 .setDescription("To start the animation, you must type the name of your role after *!roleName* ! \n For example : *!roleName RGBMASTER* !")
@@ -48,10 +48,10 @@ client.on('message', message => {
         else {
             let JSONdata ={
                 "token": data.token,
-                "roleName": arg.replace(',', ' ')
+                "roleName": arg.replace(",", " ")
             }
-            fs.writeFileSync('./data.json', JSON.stringify(JSONdata));
-            roleName = arg.replace(',', ' ');
+            fs.writeFileSync("./data.json", JSON.stringify(JSONdata));
+            roleName = arg.replace(",", " ");
             const roleNameEmbed = new Discord.MessageEmbed()
                 .setTitle("Role RGB")
                 .setDescription("Nouveau role RGB : " + roleName)
@@ -61,7 +61,7 @@ client.on('message', message => {
     }
 
     //Commande permettant de vérifier le nom du role
-    if(message.content === '!checkName') {
+    if(message.content === "!checkName") {
         const checkNameEmbed = new Discord.MessageEmbed()
             .setTitle("RoleName")
             .setDescription("The actual roleName is : " + roleName)
@@ -70,7 +70,7 @@ client.on('message', message => {
     }
 
     //Affichage d'un message d'aide pour comprendre l'utilisation du bot
-    if(message.content === '!help') {
+    if(message.content === "!help") {
         const helpEmbed = new Discord.MessageEmbed()
             .setTitle("RGBBBBBOT HELP")
             .setDescription("The **RGBBBBBOT** role must be higher than the role you want to animate !\n Then, type *!roleName* with the name of the role you want to animate ! \n And, just type *!rgb* !")
@@ -79,15 +79,15 @@ client.on('message', message => {
     }
 
     //Affichage des images submarine / dagouille / guidouille
-    if(message.content === '!submarine') {
+    if(message.content === "!submarine") {
         message.channel.send("",{files: ["./images/submarine.png"]});
     }
 
-    if(message.content === '!dagouille') {
+    if(message.content === "!dagouille") {
         message.channel.send("",{files: ["./images/dagouille.gif"]});
     }
 
-    if(message.content === '!guidouille') {
+    if(message.content === "!guidouille") {
         message.channel.send("",{files: ["./images/guidouille.gif"]});
     }
 });
